cfd9b08ffbed993bf22bf493bd5749b386dd410c
Denis Wolf <dear.mr.dw@gmail.com>
2013-11-11 23:27:51 +0200

    a little refactor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,74 +1,83 @@
 (function(){
 	'use strict';
 
-	// should be loaded as a part of configuration
-		var maxLength = '170141183460469231731687303715884105728'.length;
+	var ringViewVisualizer = {};
 
-		function validateTokenStrings(tokenStrings){
-			_.each(tokenStrings, function(tokenString){
-				if(!_.isString(tokenString)){
-					throw tokenString+' is not a String';
-				}
-				if(! /^\d+$/.test(tokenString)){
-					throw tokenString+' is not an integer';
-				}
+	// max number for Token's integer
+	ringViewVisualizer.maxLength = '170141183460469231731687303715884105728'.length;
 
-				// naive implementation}
-				if(tokenString.length > maxLength){
-					throw tokenString+' is too big';
-				}
-			});
-		}
-
-		function buildTokensCollection(params){
-			var collection = {};
-
-			params.forEach(
-				function(token){
-					if(!collection[token.length]){
-						collection[token.length] = [];
-					}
-					collection[token.length].push(token);
-				}
-			);
-
-			return collection;
-		}
-
-		function buildRulerVisualisation(parentElement, arity, tokensCollection){
-			var tokenRulerMarkings = [];
-			for (var i=1; i<=arity; i++){
-				tokenRulerMarkings.push(document.createElement('span'));
+	ringViewVisualizer._validate = function _validate(tokenStrings) {
+		_.each(tokenStrings, function (tokenString) {
+			if (!_.isString(tokenString)) {
+				throw tokenString + ' is not a String';
 			}
-			_.each(tokensCollection, function(tokenChunks, index){
-				tokenChunks
-					.map(function(tokenId){
-						var el = document.createElement('div');
-						el.setAttribute('data-token-id',tokenId);
-						return el;
-					})
-					.forEach(function(tokenElement){
-						tokenRulerMarkings[index].appendChild(tokenElement);
-					});
-			});
-			_.each(tokenRulerMarkings.reverse(), function(tokenDiv){
-				parentElement.appendChild(tokenDiv);
-			});
+			if (!/^\d+$/.test(tokenString)) {
+				throw tokenString + ' is not an integer';
+			}
+
+			// naive implementation}
+			if (tokenString.length > this.maxLength) {
+				throw tokenString + ' is too big';
+			}
+		},this);
+	};
+
+	ringViewVisualizer._buildDataCollection = function _buildDataCollection(params) {
+		var collection = {};
+		_.each(params, function (token) {
+				if (!collection[token.length]) {
+					collection[token.length] = [];
+				}
+				collection[token.length].push(token);
+			}
+		);
+		return collection;
+	};
+
+	ringViewVisualizer._createElementsByTokenRange = function _createElementByTokenRange(arity, tokensCollection) {
+		var rangesContainers = [];
+		for (var i = 1; i <= arity; i++) {
+			rangesContainers.push(document.createElement('span'));
 		}
+		_.each(tokensCollection, function addByRange (tokenChunks, index) {
+			_.each(tokenChunks, function addByToken (tokenId) {
+					var el = document.createElement('div');
+					el.setAttribute('data-token-id', tokenId);
+					rangesContainers[index].appendChild(el);
+				});
+		});
+		return rangesContainers;
+	};
 
-		var params = ['1701411834', '123', '93',
-			'3', '6923173730371588', '123', '1',
-			'234233', '69231737303715812333328',
-			'3', '69231473730371588', '1231233',
-			'1', '124124142', '124124', '12312'
-		];
+	ringViewVisualizer._createVisualisation = function _createVusialisation(container, arity, tokens){
+		var tokenRanges = this._createElementsByTokenRange(arity, tokens);
+		var parentElement = document.createElement('div');
+		_.each(tokenRanges.reverse(), function (tokenDiv) {
+			parentElement.appendChild(tokenDiv);
+		});
+		return parentElement;
+	};
 
-		validateTokenStrings(params);
+	ringViewVisualizer.generate = function generate(listOfTokens) {
+		var arity = this.maxLength,
+			container = document.createElement('div');
 
-		var tokens = buildTokensCollection(params);
+		this._validate(listOfTokens);
 
-		var container = document.body.querySelector('#container');
+		var tokens = this._buildDataCollection(listOfTokens);
+		return this._createVisualisation(container, arity, tokens);
+	};
 
-		buildRulerVisualisation(container, maxLength, tokens);
+	var params = ['1701411834', '123', '93',
+		'3', '6923173730371588', '123', '1',
+		'234233', '69231737303715812333328',
+		'3', '69231473730371588', '1231233',
+		'1', '124124142', '124124', '12312'
+	];
+
+	var container = document.body.querySelector('#container');
+
+	var visualisation = ringViewVisualizer.generate(params);
+	container.appendChild(visualisation);
 
 })();
